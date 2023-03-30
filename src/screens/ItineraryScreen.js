cf0b43354f9a3ce39e5a51bb3e404bb0e659554b
jasonaul <jason.aul@gmail.com>
2023-03-29 23:56:11 -0400

    Table styling for itinerary page

--- src/screens/ItineraryScreen.js
+++ src/screens/ItineraryScreen.js
@@ -1,12 +1,42 @@
-import { SafeAreaView, Text, View } from 'react-native';
+import { SafeAreaView, Text, View, StyleSheet } from 'react-native';
 import React from 'react'
 
 const ItineraryScreen = () => {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text className="text-red-700">ITS An ITINERARY SCREEN!</Text>
+    <View 
+    style={{ 
+      flex: 1, 
+      // alignItems: 'center', 
+      // justifyContent: 'center' 
+      }}>
+      <View style={styles.table}>
+
+      </View>
     </View>
+    
   )
 }
+
+const styles = StyleSheet.create({
+  table: {
+    flexDirection: 'column'
+  },
+  row: { 
+    flexDirection: 'row',
+  },
+  rowBlue: {
+    backgroundColor: 'blue',
+  },
+  rowWhite: {
+    backgroundColor: 'white',
+  },
+  cell: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: 'black'
+  }
+})
 
 export default ItineraryScreen
