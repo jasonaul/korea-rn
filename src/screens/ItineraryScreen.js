9d17c8f1e075b0332a524311fb0262dff9b45a49
jasonaul <jason.aul@gmail.com>
2023-03-30 00:29:46 -0400

    testing table elements

--- src/screens/ItineraryScreen.js
+++ src/screens/ItineraryScreen.js
@@ -9,8 +9,13 @@ const ItineraryScreen = () => {
       // alignItems: 'center', 
       // justifyContent: 'center' 
       }}>
+      <Text>Header?</Text>
       <View style={styles.table}>
-
+          <View style={[styles.row, styles.rowBlue]}>
+            <View style={styles.cell}><Text>Date</Text></View>
+            <View style={styles.cell}><Text>City</Text></View>
+            <View style={styles.cell}><Text><Text style={styles.bold}>Top Attractions </Text>{"\n"}Line Break Test {"\n"} Another Line Break</Text></View>
+          </View>
       </View>
     </View>
     
@@ -36,6 +41,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     borderWidth: 1,
     borderColor: 'black'
+  },
+  bold: {
+    fontWeight: 'bold'
   }
 })
 
